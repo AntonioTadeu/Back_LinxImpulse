7a83a4dee518bf76bd14d8333a45d1dfeb435cd8
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-12-02 20:33:02 -0300

    Feat: Cadastro de dados no banco
    Foi feita o cadastro de dados no banco.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,47 @@
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (0, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (1, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (2, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (3, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (4, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (5, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (6, 2, 9.99);
+
+INSERT INTO parcela(id, quantidade, valor)
+VALUES (7, 2, 9.99);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (0, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 0);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (1, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 1);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (2, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 2);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (3, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 3);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (4, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 4);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (5, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 5);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (6, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 6);
+
+INSERT INTO produto(id, nome, foto, preco_antigo, preco, descricao, parcela_id)
+VALUES (7, 'Nome do produto', '//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 23.99, 19.99, 'Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.', 7);
