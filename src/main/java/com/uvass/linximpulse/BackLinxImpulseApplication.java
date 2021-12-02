8ba8203bd2b20355b835b2dbd0b4ef8aecd8ef4b
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-12-02 18:25:34 -0300

    Feat: Primeiras configurações

---
+++ src/main/java/com/uvass/linximpulse/BackLinxImpulseApplication.java
@@ -0,0 +1,13 @@
+package com.uvass.linximpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLinxImpulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackLinxImpulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uvass/linximpulse/BackLinxImpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvass.linximpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLinxImpulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
